48e4e444de63b9c3c62ee8af508dd020bef98a5f
The Master <93973374+THEMASTERRGE@users.noreply.github.com>
2026-03-11 10:05:27 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,63 +1,37 @@
-from sqlalchemy import create_engine
+import os
+import threading
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
+from config import Config
 
-import os
+BASE = declarative_base()
 
-import threading
-import asyncio
-
-from sqlalchemy import Column, Integer, Boolean, String, ForeignKey, UniqueConstraint, func
-
-
-from sample_config import Config
-
-
-def start() -> scoped_session:
-    engine = create_engine(Config.DB_URI, client_encoding="utf8")
-    BASE.metadata.bind = engine
+def start():
+    # Fixes the rfc1738 error by ensuring a valid string is always passed
+    engine = create_engine(Config.DB_URI, connect_args={'check_same_thread': False})
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
-
-BASE = declarative_base()
 SESSION = start()
-
 INSERTION_LOCK = threading.RLock()
 
-class Thumbnail(BASE):
-    __tablename__ = "thumbnail"
+class custom_caption(BASE):
+    __tablename__ = "caption"
     id = Column(Integer, primary_key=True)
-    msg_id = Column(Integer)
+    caption = Column(String)
     
-    def __init__(self, id, msg_id):
+    def __init__(self, id, caption):
         self.id = id
-        self.msg_id = msg_id
+        self.caption = caption
 
-Thumbnail.__table__.create(checkfirst=True)
+# Ensure table exists
+custom_caption.__table__.create(bind=SESSION.bind, checkfirst=True)
 
-async def df_thumb(id, msg_id):
+# Helper methods (same as before)
+async def update_cap(id, caption):
     with INSERTION_LOCK:
-        msg = SESSION.query(Thumbnail).get(id)
-        if not msg:
-            msg = Thumbnail(id, msg_id)
-            SESSION.add(msg)
-            SESSION.flush()
-        else:
-            SESSION.delete(msg)
-            file = Thumbnail(id, msg_id)
-            SESSION.add(file)
+        cap = SESSION.query(custom_caption).get(id)
+        if cap: SESSION.delete(cap)
+        SESSION.add(custom_caption(id, caption))
         SESSION.commit()
-
-async def del_thumb(id):
-    with INSERTION_LOCK:
-        msg = SESSION.query(Thumbnail).get(id)
-        SESSION.delete(msg)
-        SESSION.commit()
-
-async def thumb(id):
-    try:
-        t = SESSION.query(Thumbnail).get(id)
-        return t
-    finally:
-        SESSION.close()
